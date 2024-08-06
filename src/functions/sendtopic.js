14e4129d1942b48b6df5e080ee36ef9551b085f2
hariramprasanth <hariramprasanth0@gmail.com>
2024-08-06 15:33:32 +0530

    Add service bus, function app and send topic

--- src/functions/sendtopic.js
+++ src/functions/sendtopic.js
@@ -5,49 +5,36 @@ const connectionString =
 	"Endpoint=sb://servicebus435.servicebus.windows.net/;SharedAccessKeyName=sendtopic1;SharedAccessKey=R257kOQ70HWPt79t9wKROj2Z2Kambd70C+ASbDl+QtQ=;EntityPath=topic1";
 const topicName = "topic1";
 
-const messages = [{ body: "India" }, { body: "Japan" }];
-
 app.http("sendtopic", {
-	methods: ["GET", "POST"],
+	methods: ["POST"],
 	authLevel: "anonymous",
 	handler: async (request, context) => {
 		context.log("-------Send Topic function triggered-----");
+
 		const sbClient = new ServiceBusClient(connectionString);
 		const sender = sbClient.createSender(topicName);
 
 		try {
-			// create a batch object
-			let batch = await sender.createMessageBatch();
-			for (let i = 0; i < messages.length; i++) {
-				// try to add the message to the batch
-				if (!batch.tryAddMessage(messages[i])) {
-					// if it fails to add the message to the current batch
-					// send the current batch as it is full
-					await sender.sendMessages(batch);
+			// Get the body of the POST request
+			const messageContent = request.body || request.text();
+			context.log("---- POST Body -----");
+            context.log(messageContent);
+            
+			// Create a message object with the body as a JSON string
+            const message = { body: JSON.stringify({ name: "hari", city : "cbe"}) }; // Hard coded messgae
 
-					// then, create a new batch
-					batch = await sender.createMessageBatch();
+			// Send the message directly to the topic
+			await sender.sendMessages(message);
 
-					// now, add the message failed to be added to the previous batch to this batch
-					if (!batch.tryAddMessage(messages[i])) {
-						// if it still can't be added to the batch, the message is probably too big to fit in a batch
-						throw new Error("Message too big to fit in a batch");
-					}
-				}
-			}
-
-			// Send the last created batch of messages to the topic
-			await sender.sendMessages(batch);
-
-			context.log(`Sent a batch of messages to the topic: ${topicName}`);
+			context.log(`Sent a message to the topic: ${topicName}`);
 		} catch (err) {
-			context.log(`Error sending messages: ${err}`);
-			return { status: 500, body: `Error sending messages: ${err.message}` };
+			context.log(`Error sending message: ${err}`);
+			return { status: 500, body: `Error sending message: ${err.message}` };
 		} finally {
 			await sender.close();
 			await sbClient.close();
 		}
 
-		return { status: 200, body: `Messages have been sent successfully to the topic: ${topicName}` };
+		return { status: 200, body: `Message has been sent successfully to the topic: ${topicName}` };
 	},
 });
